f176815ef81bf2229d035675286ad69bc96530be
Aruneko <aruneko99@gmail.com>
2018-10-25 21:48:15 +0900

    引数の数が複数のマクロと単数のマクロをうまく扱えるように変更

--- src/macro_parser.ts
+++ src/macro_parser.ts
@@ -2,9 +2,9 @@ import { Parser, ParserFunc, Success } from './parser';
 
 export class Macro {
     name: string;
-    args: Array<number>;
+    args: Array<string>;
 
-    constructor(name: string, args: Array<number>) {
+    constructor(name: string, args: Array<string>) {
         this.name = name;
         this.args = args;
     }
@@ -30,21 +30,32 @@ export class MacroParser {
     private comma = Parser.char(',');
     private position = Parser.seq([this.num, this.comma, this.num]);
 
+    private bool = Parser.choice([Parser.char('0'), Parser.char('1')])
+
     private imgplayer = this.make_macro_parser('imgplayer', this.position);
     private imgyesno = this.make_macro_parser('imgyesno', this.position);
+    private hpmax = this.make_macro_parser('hpmax', this.num);
+    private save = this.make_macro_parser('save', this.bool);
 
     private macro_parser = Parser.choice([
         this.imgplayer,
         this.imgyesno,
+        this.hpmax,
+        this.save,
     ]);
 
     parse: () => ParseResult = () => {
         const parse_result = this.macro_parser(this.message, 0);
         if (parse_result instanceof Success) {
             const macro_name: string = parse_result.result[1];
-            const raw_args: Array<string> = parse_result.result[3].filter((c: string) => c !== ",");
-            const macro_args = raw_args.map(arg => parseInt(arg));
-            return new Macro(macro_name, macro_args);
+            const raw_args = parse_result.result[3];
+            if (raw_args instanceof Array) {
+                const macro_args = raw_args.filter((c: string) => c !== ",");
+                return new Macro(macro_name, macro_args);
+            } else {
+                return new Macro(macro_name, [raw_args]);
+            }
+            
         } else {
             return this.message;
         }
