4a43b6b2ba8c9cd2ccd8cee29ae6208816469855
Aruneko <aruneko99@gmail.com>
2018-10-25 21:48:38 +0900

    hpmaxとsaveのテストを追加

--- src/macro_parser.test.ts
+++ src/macro_parser.test.ts
@@ -9,12 +9,58 @@ describe('parse macros', () => {
     const results = messages.map(message => new MacroParser(message).parse());
 
     it('check imgplayer', () => {
-        expect(results[0]).toEqual(new Macro('imgplayer', [1, 2]));
+        expect(results[0]).toEqual(new Macro('imgplayer', ['1', '2']));
     });
     it('check imgyesno', () => {
-        expect(results[1]).toEqual(new Macro('imgyesno', [4, 5]));
+        expect(results[1]).toEqual(new Macro('imgyesno', ['4', '5']));
     });
     it('check message', () => {
         expect(results[2]).toEqual('this is message!');
+    });
+});
+
+describe('parse imgplayer', () => {
+    it('success', () => {
+        const message = '$imgplayer=1,2';
+        expect(new MacroParser(message).parse()).toEqual(new Macro('imgplayer', ['1', '2']));
+    });
+    it('failure', () => {
+        const message = '$imgplayer=1';
+        expect(new MacroParser(message).parse()).toEqual(message);
+    });
+});
+
+describe('parse imgyesno', () => {
+    it('success', () => {
+        const message = '$imgyesno=1,2';
+        expect(new MacroParser(message).parse()).toEqual(new Macro('imgyesno', ['1', '2']));
+    });
+    it('failure', () => {
+        const message = '$imgyesno=1';
+        expect(new MacroParser(message).parse()).toEqual(message);
+    });
+});
+
+describe('parse hpmax', () => {
+    it('success', () => {
+        const message = '$hpmax=1';
+        expect(new MacroParser(message).parse()).toEqual(new Macro('hpmax', ['1']));
+    });
+    it('failure', () => {
+        const message = '$hpmax=hoge';
+        expect(new MacroParser(message).parse()).toEqual(message);
+    });
+});
+
+describe('parse save', () => {
+    it('success', () => {
+        let message = '$save=0';
+        expect(new MacroParser(message).parse()).toEqual(new Macro('save', ['0']));
+        message = '$save=1';
+        expect(new MacroParser(message).parse()).toEqual(new Macro('save', ['1']));
+    });
+    it('failure', () => {
+        const message = '$save=2';
+        expect(new MacroParser(message).parse()).toEqual(message);
     });
 });
